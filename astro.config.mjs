847199bc22fe0ea8110c4f7c8182b2bbed02238a
Sonu Meena <meenasonu1910@gmail.com>
2026-06-11 00:55:25 +0530

    some fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,5 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://realsalarycheck.com',
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  integrations: [
-    sitemap({
-      changefreq: 'weekly',
-      priority: 0.7,
-      customPages: ['https://realsalarycheck.com/'],
-    })
-  ]
+  output: 'static'
 });
